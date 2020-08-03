e251918524db207aaaf7a1db475144cc1761ae65
Fernando Enriquez <2013200959@untels.edu.pe>
2020-08-03 12:22:40 -0500

    VENTAS - Images y constructor agregados

--- src/java/Modelo/Productos.java
+++ src/java/Modelo/Productos.java
@@ -11,4 +11,63 @@ package Modelo;
  */
 public class Productos {
     
+    int idproducto;
+    String nomproducto;
+    String cantidad;
+    String precio;
+    String estado;
+
+    public Productos() {
+    }
+
+    public Productos(int idproducto, String nomproducto, String cantidad, String precio, String estado) {
+        this.idproducto = idproducto;
+        this.nomproducto = nomproducto;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.estado = estado;
+    }
+
+    public int getIdproducto() {
+        return idproducto;
+    }
+
+    public void setIdproducto(int idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public String getNomproducto() {
+        return nomproducto;
+    }
+
+    public void setNomproducto(String nomproducto) {
+        this.nomproducto = nomproducto;
+    }
+
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(String cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+    
 }
